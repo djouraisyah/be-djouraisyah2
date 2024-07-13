f03cac6490b0e59c2aebbebcd18f6a24a080aa61
aisyahfsr <142793713+aisyahfsr@users.noreply.github.com>
2024-07-13 13:56:51 +0700

    change connection database

--- api/config/db.js
+++ api/config/db.js
@@ -2,11 +2,11 @@ import "dotenv/config";
 import mariadb from "mariadb";
 
 export const pool = mariadb.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: roundhouse.proxy.rlwy.net,
+  port: 49813,
+  user: root,
+  password: LfidjdalfmMwMHACmLJHnscEHbrXJlqi,
+  database: railway,
 });
 
 pool
